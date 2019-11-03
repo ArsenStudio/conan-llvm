e1394b0b53d4c2abafa42c52332bb7eab8a5dbfc
intelligide <intelligide@hotmail.fr>
2019-11-02 22:55:46 -0500

    fix

--- build.py
+++ build.py
@@ -2,6 +2,14 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(reference="llvm/9.0.0")
-    # builder.add_common_builds(reference="llvm/9.0.0")
+    builder.add_common_builds(reference="llvm/9.0.0@arsen-studio/testing")
+    builder.remove_build_if(lambda build: build.settings["compiler"] == "Visual Studio" and build.options['llvm:shared'] == True)
+
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        if settings["compiler"] == "Visual Studio":
+            del options['llvm:shared']
+        filtered_builds.append([settings, options, env_vars, build_requires, reference])
+    builder.builds = filtered_builds
+
     builder.run()
--- conanfile.py
+++ conanfile.py
@@ -17,66 +17,100 @@ class LlvmConan(ConanFile):
     options = {
         'shared': [True, False],
         'enable_rtti': [True, False],
-        'build_tools': [True, False],
         'enable_pic': [True, False],
-        'enable_threads': [True, False]
+        'enable_threads': [True, False],
+        'arch_targets': "ANY",
     }
-    default_options = (
-        'shared=True',
-        'enable_rtti=True',
-        'build_tools=True',
-        'enable_pic=True',
-        'enable_threads=True',
+    default_options = {
+        'shared': True,
+        'enable_rtti': True,
+        'enable_pic': True,
+        'enable_threads': True,
+        'arch_targets': None,
         # 'gtest:shared=False'
-    )
+    }
     # requires = "gtest/1.8.0@bincrafters/stable"
 
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
+    _source_subfolder = "llvm-project"
+    _build_subfolder = "llvm-project/build"
 
     def source(self):
         if self.version not in self.conan_data["sources"]:
-            self.output.error("The version " +self.version + " is not supported.")
+            self.output.error("The version " + self.version + " is not supported.")
             exit(1)
 
-        tools.get(**self.conan_data["sources"][self.version]["llvm"])
-        os.rename("llvm-" + self.version + ".src", self._source_subfolder)
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("llvm-project-llvmorg-" + self.version, self._source_subfolder)
 
-        tools.get(**self.conan_data["sources"][self.version]["clang"], destination=os.path.join(self._source_subfolder, "tools"))
-        os.rename(os.path.join(self._source_subfolder, "tools", "cfe-" + self.version + ".src"), os.path.join(self._source_subfolder, "tools/clang"))
+        tools.replace_in_file("llvm-project/llvm/CMakeLists.txt", '''project(LLVM
+  VERSION ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}
+  LANGUAGES C CXX ASM)''',
+                              '''project(LLVM
+  VERSION ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}
+  LANGUAGES C CXX ASM)
+
+include(../../conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.shared
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.configure(build_folder=self._build_subfolder, defs={
-            'LIBCXX_INCLUDE_TESTS': False,
-            'LIBCXX_INCLUDE_DOCS': False,
+        args = None
 
+        if self.settings.compiler == "Visual Studio" and self.settings.arch == "x86_64":
+            args = ["-Thost=x64"]
+
+        defs = {
             # LLVM
-            'LLVM_INCLUDE_TOOLS': self.options.build_tools,
-            'LLVM_INCLUDE_TESTS': False,
-            'LLVM_INCLUDE_EXAMPLES': False,
-            'LLVM_INCLUDE_GO_TESTS': False,
-            'LLVM_BUILD_TOOLS': self.options.build_tools,
+            'LLVM_ENABLE_PROJECTS': 'clang',
+            'LLVM_BUILD_EXAMPLES': False,
+            #'LLVM_INCLUDE_EXAMPLES': False,
+            'LLVM_BUILD_TESTS': False,
+            #'LLVM_INCLUDE_TESTS': False,
+            'LLVM_INSTALL_CCTOOLS_SYMLINKS': False,
+            'LLVM_INSTALL_BINUTILS_SYMLINKS': False,
+
+            'LLVM_BUILD_TOOLS': False,
+
             'LLVM_ENABLE_PIC': self.options.enable_pic,
             'LLVM_ENABLE_RTTI': self.options.enable_rtti,
             'LLVM_ENABLE_THREADS': self.options.enable_threads,
-            'LLVM_BUILD_TESTS': False,
 
             # Clang
             'CLANG_INCLUDE_DOCS': False,
             'CLANG_INCLUDE_TESTS': False,
-        })
+            'CLANG_BUILD_TOOLS': False,
+        }
+
+        if self.options.arch_targets:
+            defs['LLVM_TARGETS_TO_BUILD'] = self.options.arch_targets
+
+        if self.settings.os != "Windows":
+            defs['LLVM_BUILD_LLVM_DYLIB'] = self.options.shared
+
+        cmake.configure(source_folder="llvm-project/llvm", build_folder=self._build_subfolder, args=args, defs=defs)
         return cmake
 
     def build(self):
         cmake = self._configure_cmake()
-        cmake.build()
+        cmake.build(target="clang")
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
 
+    def isPlugin(self, libname):
+        return libname.endswith("Plugin")
+
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
 
+        if self.settings.os == "Linux":
+            self.cpp_info.libs = [libname for libname in self.cpp_info.libs if not self.isPlugin(libname)]
+
+        if self.settings.os == "Windows":
+            self.cpp_info.libs.append("Version")
